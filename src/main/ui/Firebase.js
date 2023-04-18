08dda5bbcde37186c96f13654ef10a3606e6e4ab
BEATT <beattyjoshua55@Gmail.com>
2023-04-18 23:57:43 +0100

    Added setup instructions for Mobile Application project

--- src/main/ui/Firebase.js
+++ src/main/ui/Firebase.js
@@ -2,19 +2,23 @@
 
 import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Add Firebase config values
+// DO NOT COMMIT THIS
 const firebaseConfig = {
-  apiKey: "AIzaSyDco1m2MEc8cNjwnX_FZm1A6wI1EXEIbr0",
-  authDomain: "ds04-7d54b.firebaseapp.com",
-  projectId: "ds04-7d54b",
-  storageBucket: "ds04-7d54b.appspot.com",
-  messagingSenderId: "300964238925",
-  appId: "1:300964238925:web:26a8236471f06ac3012e04",
-  measurementId: "G-3FGSP3MDTF",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
---
+++ src/main/ui/src/README.md
@@ -0,0 +1,24 @@
+# CS3002 DS04 Patient Mobile Application
+
+## Setup the Project
+
+1. Create a `.env` file at `./src/main/ui/`
+    - Add a variable `REACT_APP_LOCAL_BACKEND_BASE_URL` and set to the HTTPS URL provided by [Ngrok](https://ngrok.com/)
+2. Add Firebase config values copied from [Firebase console](https://console.firebase.google.com/u/0/) to `Firebase.js` file
+    - Note: this cannot be abstracted to the `.env` file due to start up order 
+3. Open terminal and install dependencies using:
+```
+npm install
+```
+
+## Run the Application
+
+1. Open terminal and run:
+```
+expo start --clear
+```
+2. Using the ExpoGo app, scan the QR Code in the terminal
+
+## Useful commands
+
+- Use `prettier --write .` command to run Prettier formatting tool
